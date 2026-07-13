4a0ae30d2f2323fad9f3f906fc67eb547ea064ea
yashpalcodes <rathoreyashpalsingh22@gmail.com>
2026-07-13 15:59:38 +0530

    basic server created and database connected

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/dataBaseConnection');
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
